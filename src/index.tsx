0ace463add328570f9f44457c0894efa4d6e8d13
Ryan Barnes <rpbarnes9@gmail.com>
2021-05-23 10:37:41 -0700

    Auth: handle multiple redirect urls

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Amplify from "aws-amplify";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import { RecoilRoot } from 'recoil';
 
-import awsconfig from "./aws-exports";
-import "./config";
-
-Amplify.configure(awsconfig);
+import './awsConfig';
+import './config';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("page")
+    <React.StrictMode>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    </React.StrictMode>,
+    document.getElementById('page')
 );
 
 // If you want to start measuring performance in your app, pass a function
