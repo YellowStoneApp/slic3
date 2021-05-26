1931792a9e08f0d31b1b1a5d57b69cc37cff7335
Ryan Barnes <rpbarnes9@gmail.com>
2021-05-25 20:39:02 -0700

    Logging: point to datadog for front end logging

--- src/Utils/Apis/logging.service.ts
+++ src/Utils/Apis/logging.service.ts
@@ -1,45 +1,49 @@
-import logdna from "@logdna/logger";
+import logdna from '@logdna/logger';
+import { datadogLogs } from '@datadog/browser-logs';
+
+//const key = process.env.REACT_APP_DATA_DOG_INGESTION_KEY;
+const key = 'pub9fa5848296ad7c22222201297f7a12de';
+datadogLogs.init({
+    clientToken: key ?? '',
+    site: 'datadoghq.com',
+    service: 'MoreThanTheThought',
+    forwardErrorsToLogs: true,
+    sampleRate: 100,
+});
 
 // todo get client ip address for logs.
 const options: logdna.ConstructorOptions = {
-  app: "SLIC3",
+    app: 'SLIC3',
+    sendUserAgent: true,
+};
+
+const getDeviceInfo = () => {
+    const device = navigator.userAgent;
+    console.log(device);
 };
 
 const logToConsole = true;
 
-const logger = logdna.createLogger(
-  process.env.REACT_APP_LOG_DNA_INGESTION_KEY ?? "",
-  options
-);
+const logger = logdna.createLogger(process.env.REACT_APP_LOG_DNA_INGESTION_KEY ?? '', options);
 
 const info = (message: any) => {
-  logger.info(message);
-  toConsole(message);
+    datadogLogs.logger.info(message);
+    toConsole(message);
 };
 
 const error = (message: any) => {
-  logger.error(message);
-  toConsole(message);
+    datadogLogs.logger.error(message);
+    toConsole(message);
 };
 
 const warn = (message: any) => {
-  logger.warn(message);
-  toConsole(message);
-};
-
-const trace = (message: any) => {
-  logger.trace(message);
-  toConsole(message);
+    datadogLogs.logger.warn(message);
+    toConsole(message);
 };
 
 const debug = (message: any) => {
-  logger.debug(message);
-  toConsole(message);
-};
-
-const fatal = (message: any) => {
-  logger.fatal(message);
-  toConsole(message);
+    datadogLogs.logger.debug(message);
+    toConsole(message);
 };
 
 /**
@@ -47,21 +51,19 @@ const fatal = (message: any) => {
  * In the background this module should be periodically writing log message in aggregate to the server.
  */
 const flush = () => {
-  logger.flush();
+    logger.flush();
 };
 
 export const logService = {
-  trace,
-  debug,
-  info,
-  warn,
-  error,
-  fatal,
-  flush, // this maybe shouldn't be here...
+    debug,
+    info,
+    warn,
+    error,
+    flush, // this maybe shouldn't be here...
 };
 
 const toConsole = (message: any) => {
-  if (logToConsole) {
-    console.log(message);
-  }
+    if (logToConsole) {
+        console.log(message);
+    }
 };
